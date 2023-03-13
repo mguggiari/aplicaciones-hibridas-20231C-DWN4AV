b6463e6eac422e09ee498f27f389320439c9c32e
Brian Lara <brian.lara@outlook.com.ar>
2023-03-13 20:55:58 -0300

    Clase 01

---
+++ Clase 01/calculos.js
@@ -0,0 +1,9 @@
+// var nuermo = 8 // veri mal
+let numero = 5 // variable mutable
+const numero2 = 6 // variable inmutable
+
+numero = 7
+
+numero2 = 7 // error
+
+console.log("El numero es: " + numero)
